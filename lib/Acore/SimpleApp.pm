a67807e40a5762e58891edb790a54c5717e3a7a6
fujiwara <fujiwara@317c1d5b-0c06-0410-889f-8d71a4706d8e>
2009-05-13 10:35:33 +0000

    refactored: アプリケーションとコントローラのクラス分離 (SimpleApp)
    git-svn-id: http://arita/svn/Developers/fujiwara/Acore@1078 317c1d5b-0c06-0410-889f-8d71a4706d8e

--- lib/Acore/SimpleApp.pm
+++ lib/Acore/SimpleApp.pm
@@ -12,9 +12,9 @@ no Any::Moose;
     package Acore::SimpleApp::Dispatcher;
     use HTTPx::Dispatcher;
     connect "",
-        { controller => "Acore::SimpleApp", action => "dispatch_index" };
+        { controller => "Acore::SimpleApp::Controller", action => "dispatch_index" };
     connect "adoc/:path",
-        { controller => "Acore::SimpleApp", action => "dispatch_acore" };
+        { controller => "Acore::SimpleApp::Controller", action => "dispatch_acore" };
     connect "static/:filename",
         { controller => "Acore::SimpleApp", action => "dispatch_static" };
     connect "favicon.ico",
@@ -22,35 +22,9 @@ no Any::Moose;
     connect "admin_console/:action",
         { controller => "Acore::WAF::Controller::AdminConsole" };
     connect ":action",
-        { controller => "Acore::SimpleApp", };
+        { controller => "Acore::SimpleApp::Controller", };
 }
 
 __PACKAGE__->setup(qw/ Session /);
-
-sub null {
-    my ($self, $c) = @_;
-    $c->res->body("ok");
-}
-
-sub dispatch_index {
-    my ($self, $c) = @_;
-    $c->log->info("dispatch index");
-    $c->prepare_acore();
-
-    my $count = $c->session->get('counter');
-    $c->session->set( counter => ++$count );
-
-    $c->render("index.mt");
-}
-
-sub dispatch_acore {
-    my ($self, $c, $args) = @_;
-    $c->serve_acore_document( "/" . $args->{path} )
-        or do {
-            $c->res->status(404);
-            $c->res->body("Not found / acore document");
-        };
-}
-
 
 1;
