fe0ea48f9cadf6fb8fca2ec41027ad10c355f781
fujiwara <fujiwara@317c1d5b-0c06-0410-889f-8d71a4706d8e>
2009-06-01 11:48:26 +0000

    Plugin::OpenID 追加
    git-svn-id: http://arita/svn/Developers/fujiwara/Acore@1203 317c1d5b-0c06-0410-889f-8d71a4706d8e

---
+++ lib/Acore/WAF/Plugin/OpenID.pm
@@ -0,0 +1,85 @@
+package Acore::WAF::Plugin::OpenID;
+
+use strict;
+use warnings;
+
+our @EXPORT = qw/ authenticate_openid /;
+
+sub authenticate_openid {
+    my $c = shift;
+
+    my $claimed_uri = $c->req->param('openid_identifier');
+    unless ($claimed_uri or $c->req->param('openid-check')) {
+        return;
+    }
+
+    require Net::OpenID::Consumer;
+    require LWPx::ParanoidAgent;
+
+    my $version = Net::OpenID::Consumer->VERSION;
+    if ($version < 1.03) {
+        $c->log->warning("Net::OpenID::Consumer->VERSION $version, Can't use OpenID 2.0 (e.g. yahoo.co.jp or mixi.jp ...)");
+    }
+
+    my $config  = $c->config->{openid};
+    my $secret  = $config->{consumer_secret} || do {
+        my $s = join '+', __PACKAGE__,
+            ref( $c ), sort keys %{ $c->config };
+        $s = substr($s, 0, 255) if length $s > 255;
+        $s;
+    };
+    $c->log->debug("comsumer secret=$secret");
+    my $ua      = LWPx::ParanoidAgent->new;
+    my $args    = {};
+    $args->{$_} = $c->req->param($_) for $c->req->param;
+    $c->log->debug( Data::Dumper::Dumper $args );
+
+    my $csr = Net::OpenID::Consumer->new(
+        ua              => $ua,
+        args            => $args,
+        consumer_secret => $secret,
+    );
+    local *LWP::Debug::debug = sub { $c->log->debug("LWP::Debug::debug @_") };
+    local *LWP::Debug::trace = *LWP::Debug::debug;
+
+    if ($claimed_uri) {
+        my $current = $c->uri_for( $c->req->uri->path );
+
+        my $identity = eval { $csr->claimed_identity($claimed_uri) }
+            or $c->error( 500 => "OpenID error: " . $csr->err );
+
+        my $check_url = $identity->check_url(
+            return_to      => $current . '?openid-check=1',
+            trust_root     => $current,
+            delayed_return => 1,
+        );
+        $c->redirect($check_url);
+        $c->detach;
+    }
+    elsif ($c->req->param('openid-check')) {
+        $c->log->debug("openid-check");
+        if (my $setup_url = $csr->user_setup_url) {
+            $c->log->debug("setup_url=$setup_url");
+            $c->redirect($setup_url);
+            $c->detach;
+        }
+        elsif ($csr->user_cancel) {
+            $c->log->error('user_cancel');
+        }
+        elsif (my $identity = $csr->verified_identity) {
+            $c->log->debug("verified_identity");
+            my $user = +{ map { $_ => scalar $identity->$_ }
+                    qw(url display rss atom foaf declared_rss
+                       declared_atom declared_foaf foafmaker) };
+            $c->user($user);
+            return $user;
+        }
+        else {
+            $c->error( 500 => "Error validating identity: " . $csr->err );
+        }
+    }
+
+    return;
+}
+
+1;
