f91b354a8bd8e8b2a231725752781f0286b30383
fujiwara <fujiwara@317c1d5b-0c06-0410-889f-8d71a4706d8e>
2009-07-14 07:09:45 +0000

    MinimalCGI の upload 関連の挙動を他と揃える
    git-svn-id: http://arita/svn/Developers/fujiwara/Acore@1487 317c1d5b-0c06-0410-889f-8d71a4706d8e

--- lib/Acore/WAF/MinimalCGI.pm
+++ lib/Acore/WAF/MinimalCGI.pm
@@ -4,7 +4,13 @@ use strict;
 use warnings;
 use URI;
 {
-    package HTTP::Engine::Request;
+    package # hide from pause
+        HTTP::Engine::Request;
+    use HTTP::Engine::Request::Upload;
+
+    $CGI::Simple::DISABLE_UPLOADS = 0;
+    $CGI::Simple::POST_MAX        = 1024 * 1024 * 10;
+
     sub uri  { $ENV{PATH_INFO} }
     sub path { $ENV{PATH_INFO} }
     sub base {
@@ -23,6 +29,66 @@ use URI;
 
     no warnings 'redefine';
     sub method { $ENV{REQUEST_METHOD} || 'GET' }
+
+    sub uploads {
+        my $self = shift;
+        $self->{uploads} ||= $self->_prepare_uploads;
+    }
+
+    sub upload {
+        my $self = shift;
+        return keys %{ $self->uploads } if @_ == 0;
+
+        if (@_ == 1) {
+            my $upload = shift;
+            return wantarray ? () : undef unless exists $self->uploads->{$upload};
+            if (ref $self->uploads->{$upload} eq 'ARRAY') {
+                return (wantarray)
+                    ? @{ $self->uploads->{$upload} }
+                    : $self->uploads->{$upload}->[0];
+            } else {
+                return (wantarray)
+                    ? ( $self->uploads->{$upload} )
+                    : $self->uploads->{$upload};
+            }
+        }
+    }
+
+    sub _prepare_uploads {
+        my $self = shift;
+
+        $self->{cs} ||= CGI::Simple->new();
+        my $q = $self->{cs};
+
+        my %uploads;
+        for my $name ( keys %{ $q->{".upload_fields"} } ) {
+            my $filename = $q->{".upload_fields"}->{$name};
+            my @uploads;
+            my $headers = HTTP::Headers::Fast->new();
+            push(
+                @uploads,
+                HTTP::Engine::Request::Upload->new(
+                    headers  => $headers,
+                    fh       => $q->upload($filename),
+                    size     => $q->upload_info($filename, 'size'),
+                    filename => $filename,
+                )
+            );
+            $uploads{$name} = @uploads > 1 ? \@uploads : $uploads[0];
+        }
+        return \%uploads;
+    }
+}
+
+{
+    package # hide from pause
+        HTTP::Engine::Request::Upload;
+    no warnings "redefine";
+    sub copy_to {
+        my $self = shift;
+        require File::Copy;
+        File::Copy::copy( $self->fh, @_ );
+    }
 }
 
 1;
